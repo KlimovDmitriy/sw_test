9984264df9c8fe4db78aa57c0857ee967231df3f
KlimovDmitriy <59648283+KlimovDmitriy@users.noreply.github.com>
2022-08-16 22:04:36 +0300

    Добавление билда для дэшборда

--- buildSrc/dashboard/src/services/chartService.js
+++ buildSrc/dashboard/src/services/chartService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3010/api/dashboard'
+const baseUrl = '/api/dashboard'
 
 const getEventsData = async () => {
   const response = await axios.get(baseUrl);
